cd33f5e9d1720db1f9ff87493f438dde1f19e50e
Jason Luo <yulaie1012@gmail.com>
2024-01-09 11:09:20 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { atom, useAtom } from 'jotai';
+
+const textAtom = atom('hello');
+const lengthAtom = atom(get => get(textAtom).length);
+const upperCaseAtom = atom(get => get(textAtom).toUpperCase());
+
 const Input = () => {
-  return <input defaultValue={'hello'} onChange={e => console.log(e.target.value)} />;
+  const [text, setText] = useAtom(textAtom);
+  return <input value={text} onChange={e => setText(e.target.value)} />;
 };
 
 const CharCount = () => {
-  return <div>Length: 5</div>;
+  const [len] = useAtom(lengthAtom);
+  return <div>Length: {len}</div>;
 };
 
 const UpperCase = () => {
-  return <div>Upper Case: HELLO</div>;
+  const [upperCase] = useAtom(upperCaseAtom);
+  return <div>Upper Case: {upperCase}</div>;
 };
 
 const App = () => {
